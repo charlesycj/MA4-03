808a0c418477062aca88f8b678d2010682cc9116
염철진 <josg0921@gmail.com>
2025-05-20 19:16:01 +0900

    25-05-20 19:15
    말 설치 스크립트

--- Assets/Script/RayCastTest.cs
+++ Assets/Script/RayCastTest.cs
@@ -1,25 +1,69 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class RayCastTest : MonoBehaviour
+public class PiecePlacer : MonoBehaviour
 {
-    private Camera mainCamera;
+    public GameObject ghostPrefab;
+    public GameObject piecePrefab;
+    public Grid grid;
+
+   
+
+    private GameObject ghostInstance;
 
     void Start()
     {
-        mainCamera = Camera.main;
+        if (grid == null)
+            grid = FindObjectOfType<Grid>();
+
+        ghostInstance = Instantiate(ghostPrefab);
+        SetGhostTransparency(0.5f);
     }
 
     void Update()
     {
-        Vector2 mousePos = Mouse.current.position.ReadValue();
-        Ray ray = mainCamera.ScreenPointToRay(mousePos);
-        RaycastHit hit;
+        Vector3 mouseScreenPos = Input.mousePosition;
+        mouseScreenPos.z = 10f;
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
 
-        if (Physics.Raycast(ray, out hit))
+        Vector3Int rawCellPos = grid.WorldToCell(mouseWorldPos);
+
+        int boardCellX = rawCellPos.x / 2;
+        int boardCellY = rawCellPos.y / 2;
+
+        Vector3Int snappedCellPos = new Vector3Int(boardCellX * 2, boardCellY * 2, 0);
+        Vector3 cellCenterPos = grid.GetCellCenterWorld(snappedCellPos) + new Vector3(1f, 1f, 0f);
+
+        // 보드 영역 밖이면 고스트 숨기기
+        if (boardCellX < -5 || boardCellX > 4||
+            boardCellY < -4 || boardCellY > 5)
         {
-            GameObject target = hit.collider.gameObject;
-            Debug.Log("현재 커서 아래: " + target.name);
+            ghostInstance.SetActive(false);
+            return;
+        }
+        else
+        {
+            ghostInstance.SetActive(true);
+        }
+
+        ghostInstance.transform.position = cellCenterPos;
+
+        //if (ghostInstance != null)
+        //    ghostInstance.transform.position = cellCenterPos;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(piecePrefab, cellCenterPos, Quaternion.identity);
+        }
+    }
+
+    void SetGhostTransparency(float alpha)
+    {
+        var sr = ghostInstance.GetComponent<SpriteRenderer>();
+        if (sr != null)
+        {
+            Color c = sr.color;
+            c.a = alpha;
+            sr.color = c;
         }
     }
 }
